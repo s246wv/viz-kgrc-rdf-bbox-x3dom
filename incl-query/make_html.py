8f649394ffb95b4463d61914266de86270768fdb
s246wv <satoshi.nishimura.3194@gmail.com>
2022-10-10 21:23:00 +0900

    done A and C

--- incl-query/make_html.py
+++ incl-query/make_html.py
@@ -55,99 +55,114 @@ def Route_maker(key):
 
     return routes
 
-html = ET.Element('html')
-# headべた書き
-head = ET.SubElement(html, 'head')
-meta = ET.SubElement(
-    head, 'meta', {'http-equiv': 'X-UA-Compatible', 'content': 'IE=edge'})
-title = ET.SubElement(head, 'title')
-title.text = "KGRC-RDFのbboxを描いてみた"
-script = ET.SubElement(head, 'script', {
-                       'type': 'text/javascript', 'src': 'https://www.x3dom.org/download/x3dom.js'})
-script.text = ' '
-link = ET.SubElement(head, 'link', {
-                     'rel': 'stylesheet', 'type': 'text/css', 'href': 'https://www.x3dom.org/download/x3dom.css'})
-link.text = ' '
+def header_maker(html):
+    # headべた書き
+    head = ET.SubElement(html, 'head')
+    meta = ET.SubElement(
+        head, 'meta', {'http-equiv': 'X-UA-Compatible', 'content': 'IE=edge'})
+    title = ET.SubElement(head, 'title')
+    title.text = "KGRC-RDFのbboxを描いてみた"
+    script = ET.SubElement(head, 'script', {
+                        'type': 'text/javascript', 'src': 'https://www.x3dom.org/download/x3dom.js'})
+    script.text = ' '
+    link = ET.SubElement(head, 'link', {
+                        'rel': 'stylesheet', 'type': 'text/css', 'href': 'https://www.x3dom.org/download/x3dom.css'})
+    link.text = ' '
 
-# bodyとhtml要素
-body = ET.SubElement(html, 'body')
-sectionLabel = ET.SubElement(body, 'h1')
-sectionLabel.text = "virtualhome2kg-Admire_paintings.ttlのbboxを描いてみる"
-descriptionLabel = ET.SubElement(body, 'p')
-descriptionLabel.text = "操作方法は下の方にあります．マウスでくるくる回せます．キャラクターだけ赤色，他は透明な緑色です．動いてますが，動画と時間はあっていません．バウンディングボックスのサイズは初期値のままです．キャラクタがかがんだり方向転換していたりしても変わりません．"
-
-# ここから中身
-x3d = ET.SubElement(body, 'x3d', {'width': '1920px', 'height': '1080px'})
-scene = ET.SubElement(x3d, 'scene')
-ET.SubElement(scene, 'environment', {'smallFeatureCulling': 'true'}).text = " "
-
-# first situation
-# sizeをもらわないといけない．
-with open("situation_1.srj", "r") as f:
-    firstSituation = json.load(f)
-
-objectDict = {}
-
-for row in firstSituation["results"]["bindings"]:
-    objectDict[row["objectLabel"]["value"] + row["objectId"]["value"]] = {
-        "bboxSize": (row["BSX"]["value"], row["BSY"]["value"], row["BSZ"]["value"]),
-        "keyValue": [(row["BCX"]["value"], row["BCY"]["value"], row["BCZ"]["value"])],  # ここはappendで追加する．
-        "key": [0],  # ここもappendでkeyのlengthを追加する．
-        "objectType": row["objectLabel"]["value"],
-    }
-
-
-# 残りのsituation
-# sizeは一個目そのまま．繰り返しで足していく．
-fileList = ["situation_2.srj", "situation_3.srj", "situation_4.srj"]
-for file in fileList:
-    with open(file, "r") as f:
-        nextSituation = json.load(f)
-    for row in nextSituation["results"]["bindings"]:
-        objectDict[row["objectLabel"]["value"] + row["objectId"]["value"]]["keyValue"].append((row["BCX"]["value"], row["BCY"]["value"], row["BCZ"]["value"]))
-        objectDict[row["objectLabel"]["value"] + row["objectId"]["value"]]["key"].append(len(objectDict[row["objectLabel"]["value"] + row["objectId"]["value"]]["key"]))
-
-# pprint.pprint(objectDict)
-
-# nodeを作ります．
-# タイムセンサーが先
-timeSensor = timeSensor_maker(4)
-scene.append(timeSensor)
-# 物は後
-for o in objectDict:
-    transform = transform_maker(o, objectDict[o])
-    scene.append(transform)
-    positionInterpolator = PositionInterpolator_maker(o, objectDict[o])
-    scene.append(positionInterpolator)
-    routes = Route_maker(o)
-    for route in routes:
-        scene.append(route)
+def body_maker(html):
+    # bodyとhtml要素
+    body = ET.SubElement(html, 'body')
+    sectionLabel = ET.SubElement(body, 'h1')
+    sectionLabel.text = "virtualhome2kg-Admire_paintings.ttlのbboxを描いてみる"
+    descriptionLabel = ET.SubElement(body, 'p')
+    descriptionLabel.text = "操作方法は下の方にあります．マウスでくるくる回せます．キャラクターだけ赤色，他は透明な緑色です．動いてますが，動画と時間はあっていません．バウンディングボックスのサイズは初期値のままです．キャラクタがかがんだり方向転換していたりしても変わりません．"
     
-## 操作方法の説明
-ET.SubElement(body, "h4").text = "操作方法"
-table = ET.SubElement(body, 'table')
-tr = ET.SubElement(table, 'tr')
-ET.SubElement(tr, 'th').text = "機能"
-ET.SubElement(tr, 'th').text = "マウスボタン"
-tbody = ET.SubElement(table, 'tbody')
-tr1 = ET.SubElement(tbody, 'tr')
-ET.SubElement(tr1, 'td').text = "回転"
-ET.SubElement(tr1, 'td').text = "左 / 左 + Shift"
-tr2 = ET.SubElement(tbody, 'tr')
-ET.SubElement(tr2, 'td').text = "パン"
-ET.SubElement(tr2, 'td').text = "真ん中 / 左 + Ctrl"
-tr3 = ET.SubElement(tbody, 'tr')
-ET.SubElement(tr3, 'td').text = "ズーム"
-ET.SubElement(tr3, 'td').text = "右 / ホイール / 左 + Alt"
-tr4 = ET.SubElement(tbody, 'tr')
-ET.SubElement(tr4, 'td').text = "描画対象が真ん中に来るようにする"
-ET.SubElement(tr4, 'td').text = "左をダブルクリック"
-tr5 = ET.SubElement(tbody, 'tr')
-ET.SubElement(tr5, 'td').text = "ビューを初期値に戻す"
-ET.SubElement(tr5, 'td').text = "キーボードのA"
-tr6 = ET.SubElement(tbody, 'tr')
-ET.SubElement(tr6, 'td').text = "小さいものも表示する"
-ET.SubElement(tr6, 'td').text = "キーボードのS"
+    return body
 
-tree = ET.ElementTree(html)
-tree.write('virtualhome2kg-Admire_paintings.html', encoding='utf-8')
+def usage_maker(body):
+    ## 操作方法の説明
+    ET.SubElement(body, "h4").text = "操作方法"
+    table = ET.SubElement(body, 'table')
+    tr = ET.SubElement(table, 'tr')
+    ET.SubElement(tr, 'th').text = "機能"
+    ET.SubElement(tr, 'th').text = "マウスボタン"
+    tbody = ET.SubElement(table, 'tbody')
+    tr1 = ET.SubElement(tbody, 'tr')
+    ET.SubElement(tr1, 'td').text = "回転"
+    ET.SubElement(tr1, 'td').text = "左 / 左 + Shift"
+    tr2 = ET.SubElement(tbody, 'tr')
+    ET.SubElement(tr2, 'td').text = "パン"
+    ET.SubElement(tr2, 'td').text = "真ん中 / 左 + Ctrl"
+    tr3 = ET.SubElement(tbody, 'tr')
+    ET.SubElement(tr3, 'td').text = "ズーム"
+    ET.SubElement(tr3, 'td').text = "右 / ホイール / 左 + Alt"
+    tr4 = ET.SubElement(tbody, 'tr')
+    ET.SubElement(tr4, 'td').text = "描画対象が真ん中に来るようにする"
+    ET.SubElement(tr4, 'td').text = "左をダブルクリック"
+    tr5 = ET.SubElement(tbody, 'tr')
+    ET.SubElement(tr5, 'td').text = "ビューを初期値に戻す"
+    ET.SubElement(tr5, 'td').text = "キーボードのA"
+    tr6 = ET.SubElement(tbody, 'tr')
+    ET.SubElement(tr6, 'td').text = "小さいものも表示する"
+    ET.SubElement(tr6, 'td').text = "キーボードのS"
+
+def main():
+    html = ET.Element('html')
+    ## ヘッダとボディの最初
+    header_maker(html)
+    body = body_maker(html)
+
+    # ここから中身
+    x3d = ET.SubElement(body, 'x3d', {'width': '1920px', 'height': '1080px'})
+    scene = ET.SubElement(x3d, 'scene')
+    ET.SubElement(scene, 'environment', {'smallFeatureCulling': 'true'}).text = " "
+
+    # first situation
+    # sizeをもらわないといけない．
+    with open("situation_1.srj", "r") as f:
+        firstSituation = json.load(f)
+
+    objectDict = {}
+
+    for row in firstSituation["results"]["bindings"]:
+        objectDict[row["objectLabel"]["value"] + row["objectId"]["value"]] = {
+            "bboxSize": (row["BSX"]["value"], row["BSY"]["value"], row["BSZ"]["value"]),
+            "keyValue": [(row["BCX"]["value"], row["BCY"]["value"], row["BCZ"]["value"])],  # ここはappendで追加する．
+            "key": [0],  # ここもappendでkeyのlengthを追加する．
+            "objectType": row["objectLabel"]["value"],
+        }
+
+
+    # 残りのsituation
+    # sizeは一個目そのまま．繰り返しで足していく．
+    fileList = ["situation_2.srj", "situation_3.srj", "situation_4.srj"]
+    for file in fileList:
+        with open(file, "r") as f:
+            nextSituation = json.load(f)
+        for row in nextSituation["results"]["bindings"]:
+            objectDict[row["objectLabel"]["value"] + row["objectId"]["value"]]["keyValue"].append((row["BCX"]["value"], row["BCY"]["value"], row["BCZ"]["value"]))
+            objectDict[row["objectLabel"]["value"] + row["objectId"]["value"]]["key"].append(len(objectDict[row["objectLabel"]["value"] + row["objectId"]["value"]]["key"]))
+
+    # pprint.pprint(objectDict)
+
+    # nodeを作ります．
+    # タイムセンサーが先
+    timeSensor = timeSensor_maker(4)
+    scene.append(timeSensor)
+    # 物は後
+    for o in objectDict:
+        transform = transform_maker(o, objectDict[o])
+        scene.append(transform)
+        positionInterpolator = PositionInterpolator_maker(o, objectDict[o])
+        scene.append(positionInterpolator)
+        routes = Route_maker(o)
+        for route in routes:
+            scene.append(route)
+    ## 操作方法の説明  
+    usage_maker(body)
+
+    tree = ET.ElementTree(html)
+    tree.write('virtualhome2kg-Admire_paintings.html', encoding='utf-8')
+
+if __name__ == "__main__":
+    main()
